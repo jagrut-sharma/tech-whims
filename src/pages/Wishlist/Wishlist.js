9ca8da67a1d1649c8062e7fb9eba38ca47778e3d
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-06-05 22:39:23 +0530

    :sparkles:  feat: added wishlist

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import classes from "./Wishlist.module.css";
 import { useDataContext } from "../../context/DataContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 export default function Wishlist() {
   const {
@@ -10,13 +11,20 @@ export default function Wishlist() {
 
   return (
     <>
-      <main>
-        <h1>Wishlist</h1>
+      <main className={classes["wishlist-container"]}>
+        <h2>Wishlist ({wishlist.length})</h2>
+
         {wishlist.length === 0 && (
           <p className={classes["empty-alert"]}>
             Please add items to see your wishlist
           </p>
         )}
+
+        <div className={classes["wishlist-product-container"]}>
+          {wishlist.map((product) => (
+            <ProductCard {...product} key={product._id} />
+          ))}
+        </div>
       </main>
     </>
   );
